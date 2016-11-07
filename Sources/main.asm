bcf4c44e141afd26ffc6cf874fc58ca1ba77b9a1
JulianSB <julian.9621@gmail.com>
2016-11-06 21:14:22 -0500

    i/o Line passing XBEE MODULE

--- Sources/main.asm
+++ Sources/main.asm
@@ -13,24 +13,36 @@ _Startup:
             TXS
             CLRA 
             STA SOPT1
+            MOV #$FF,PTEDD
+            BSET 2,PTDDD
+            BSET 2,PTDD
+
 
 mainLoop:					; Dincer Aidyn
 
          	CLRA 
          	STA SCI2BDH 
-         	LDA #60  ; Setting baud rate to 9600 bps
+         	LDA #60 ; Setting baud rate to 9600 bps
          	STA SCI2BDL 
          	
          	MOV #%00000000,SCI2C1 ; 
          	MOV #%00000000,SCI2C3 ; No error flags
-         	MOV #$0C,SCI2C2 ; Enabling transmission
-         	
-yoyis      	BRCLR 7,SCI2S1,*
-         	MOV #25D,var_delay;| Blink LED
-		    JSR delayAx5ms
-		    LDA #'a'
-         	STA SCI2D  
-			BRA yoyis
+         	;MOV #$0C,SCI2C2 ; Enabling transmission and reception
+         	BSET SCI1C2_RE,SCI2C2; Enabling data reception
+         		 
+	 									; This part is for data reception 
+delta 		BRCLR SCI1S1_RDRF,SCI2S1,* 			; Wait for data	
+	 		LDA	SCI2D	
+	 		CBEQA #'›',led_off
+	 		CBEQA #'Õ',led_on
+	 		BRA delta 
+led_on:
+		    BSET 2,PTDD
+		    BRA delta
+led_off:	
+			BCLR 2,PTDD  ;~¢@1UT∆È› 
+			BRA delta
+	     				 		 
             BRA    mainLoop
 
 ;************************************************************************SUBROUTINE FOR DELAY GENERATION
